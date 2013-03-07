9e55aeba24a82090b9a08d97b604a64548fd57a3
dmdu <duplyakin@gmail.com>
2013-03-07 06:25:12 -0700

    walltimes continued

--- resources/jobs.py
+++ resources/jobs.py
@@ -1,4 +1,6 @@
 import logging
+import time
+import datetime
 from lib.util import RemoteCommand
 
 LOG = logging.getLogger(__name__)
@@ -11,8 +13,29 @@ class Job(object):
         self.running = running
         self.node = node
 
-        self.walltime = None
-        self.progress = None
+        self.walltime = None # TODO: Get it from the dict
+        self.progress = self.calculate_progress()
+
+    def calculate_progress(self):
+        # Walltime should be seconds
+        # Convert runtime to seconds. Runtime is reported in format: 0+00:01:40
+        if '+' in self.running:
+            [d, hms] = self.running.split('+')
+            d_seconds = int(d)*86400
+            hms_time = time.strptime(hms,'%H:%M:%S')
+            hms_seconds = datetime.timedelta(hours=hms_time.tm_hour,minutes=hms_time.tm_min,seconds=hms_time.tm_sec).total_seconds()
+            runtime_seconds = d_seconds + hms_seconds
+            if not self.walltime:
+                LOG.error("Error in calculating progress for job %s. Walltime is unknown" % (self.id))
+                return 0
+            if runtime_seconds <= self.walltime:
+                return float(runtime_seconds)/self.walltime
+            else:
+                LOG.error("Error in calculating progress for job %s. Runtime(%ds) > Walltime(%ds)" % (self.id, runtime_seconds, self.walltime))
+                return 1.0
+        else:
+            LOG.error("Error in parsing info for job %s. Can't find '+' in job's runtime: %s"  % (self.id, self.running))
+            return 0
 
 class Jobs(object):
 
@@ -71,4 +94,5 @@ class Jobs(object):
             return 0
         else:
             return int(rcmd.stdout)
+
 
--- resources/walltimes.py
+++ resources/walltimes.py
@@ -5,5 +5,23 @@ LOG = logging.getLogger(__name__)
 class Walltimes(object):
 
     def __init__(self, config):
-
         self.config = config
+
+    def get_walltimes_raw(self):
+        cmd_walltimes = "condor_q -format \"%d.\" ClusterId -format \"%d \" ProcId -format \"%s\\n\" Args"
+        return self.exec_cmd_on_condor_master(cmd_walltimes)
+
+    def save_walltimes_dict(self):
+        self.walltimes_dict = {}
+        out = self.get_walltimes_raw()
+        items = out.split()
+        if len(items) % 2:
+            LOG.error("Odd number of elements in the job-walltime list")
+            return
+        for i in range(0, len(items), 2):
+            job_id = items[i]
+            job_walltime = items[i+1]
+            self.walltimes_dict[job_id] = job_walltime
+        LOG.info("Walltimes dictionary is saved. Number of jobs: %d" % (len(self.walltimes_dict)))
+
+    
